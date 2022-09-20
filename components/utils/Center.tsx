304fc27d5887609ad05d7c49dbe17d0d7f54a869
Damian Dzialo <ddzialo28@interia.pl>
2022-09-20 14:12:20 +0200

    Attach loading spinner to home page

---
+++ components/utils/Center.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export default Center;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,38 @@ import "normalize.css/normalize.css";
 import "../styles/global.css";
 import "../styles/nprogress.css";
 import { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import Spinner from "components/utils/Spinner";
+import Wrapper from "styles/Wrapper";
+import WideWrapper from "styles/WideWrapper";
+import Center from "components/utils/Center";
 
 // eslint-disable-next-line react/prop-types
 const App = ({ Component, pageProps }: AppProps) => {
   const apolloClient = useApollo(pageProps);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
 
   return (
-    <ApolloProvider client={apolloClient as ApolloClient<any>}>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
-    </ApolloProvider>
+    <>
+      {!loading ? (
+        <ApolloProvider client={apolloClient as ApolloClient<any>}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
+      ) : (
+        <Center>
+          <Spinner />
+        </Center>
+      )}
+    </>
   );
 };
 
